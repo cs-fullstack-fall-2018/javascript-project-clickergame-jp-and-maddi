89cc32a8314e2e7ff6a36fa768c0876e9193bd17
Jordan Parker <parkerj4321@gmail.com>
2018-08-21 11:04:49 -0500

    Have the main click button working

--- JS.js
+++ JS.js
@@ -1,15 +1,19 @@
-var score = 0;
+var score = document.getElementById('counter');
+var clickButton = document.getElementById("clicker");
+var sum = 0;
 
-//this function resets the game and starts at the beginning
-function reset(){
-    document.getElementById("multi").reset();
-    document.getElementById("auto").reset();
-}
+//
+// //this function resets the game and starts at the beginning
+// function reset(){
+//     document.getElementById("multi").reset();
+//     document.getElementById("auto").reset();
+// }
 
 
 //this function determines what happens when the clicker is clicked (add 1 for every click)
 function clicker(){
-    var sum = 0;
     sum += 1;
-    score = sum;
+    score.innerHTML = sum;
 }
+
+clickButton.addEventListener("click", clicker);
